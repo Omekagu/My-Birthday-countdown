cc4190360290992d9f0fe08f094db09a8c358748
Omekagu <105523996+Omekagu@users.noreply.github.com>
2022-06-13 12:15:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const daysEL = document.getElementById("days");
+const hoursEL = document.getElementById("hours");
+const minsEL = document.getElementById("mins");
+const secondsEL = document.getElementById("seconds");
+const myBirthday = "12 sept 2022";
+
+function countdown() {
+  const myBirthdayDate = new Date(myBirthday);
+  const currentDate = new Date();
+
+  const totalSeconds = (myBirthdayDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+
+  daysEL.innerHTML = formatTime(days);
+  hoursEL.innerHTML = formatTime(hours);
+  minsEL.innerHTML = formatTime(mins);
+  secondsEL.innerHTML = formatTime(seconds);
+}
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+countdown();
+setInterval(countdown, 1000);
